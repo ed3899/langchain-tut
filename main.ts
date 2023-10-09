9daef6fe7d7f058bc2fae8fb2d1c92310ef8006b
Eduardo Casanova <53999059+ed3899@users.noreply.github.com>
2023-10-09 21:42:02 +0000

    chheck

--- main.ts
+++ main.ts
@@ -1,31 +1,8 @@
 import {config} from "dotenv";
 
 config();
-
-import {
-  Browser,
-  Page,
-  PuppeteerWebBaseLoader,
-} from "langchain/document_loaders/web/puppeteer";
-import {LLMChain} from "langchain/chains";
-import {OpenAI} from "langchain/llms/openai";
 import {ChatOpenAI} from "langchain/chat_models/openai";
-import {HumanMessage, LLMResult} from "langchain/schema";
-import {StructuredOutputParser} from "langchain/output_parsers";
-import {
-  ChatPromptTemplate,
-  ConditionalPromptSelector,
-  FewShotPromptTemplate,
-  HumanMessagePromptTemplate,
-  PromptTemplate,
-  SystemMessagePromptTemplate,
-} from "langchain/prompts";
-import {
-  BaseOutputParser,
-  FormatInstructionsOptions,
-} from "langchain/schema/output_parser";
-import {Serialized} from "langchain/dist/load/serializable";
-import {CheerioWebBaseLoader} from "langchain/document_loaders/web/cheerio";
+
 import {compile} from "html-to-text";
 import {RecursiveUrlLoader} from "langchain/document_loaders/web/recursive_url";
 import {RecursiveCharacterTextSplitter} from "langchain/text_splitter";
@@ -37,315 +14,21 @@ import {OpenAIEmbeddings} from "langchain/embeddings/openai";
 import * as path from "path";
 import {ConversationalRetrievalQAChain} from "langchain/chains";
 import {BufferMemory} from "langchain/memory";
-import {Document} from "langchain/document";
-
-// const llm = new OpenAI({
-//   openAIApiKey: process.env.OPENAI_API_KEY,
-//   temperature: 0.9,
-//   maxRetries: 10,
-//   maxConcurrency: 5,
-//   cache: true,
-//   maxTokens: 25,
-//   modelName: 'ada-code-search-code'
-// });
-
-// const chatModel = new ChatOpenAI();
-
-// const text =
-//   "What would be a good company name for a company that makes colorful socks?";
-
-// const prediction1 = async () => {
-//   const llmResult = await llm.predict(text);
-//   const chatModelResult = await chatModel.predict(text);
-
-//   console.log("Prediction 1");
-//   console.log(llmResult);
-//   console.log(chatModelResult);
-// };
-
-// const prediction2 = async () => {
-//   const messages = [new HumanMessage({content: text})];
-
-//   const llmResult = await llm.predictMessages(messages);
-//   const chatModelResult = await chatModel.predictMessages(messages);
-
-//   console.log("Prediction 2");
-//   console.log(llmResult);
-//   console.log(chatModelResult);
-// };
-
-// const prediction3 = async () => {
-//   const prompt = PromptTemplate.fromTemplate(
-//     "What is a good name for a company that makes {product}?"
-//   );
-
-//   const formattedPrompt = await prompt.format({
-//     product: "colorful socks",
-//   });
-
-//   console.log("Prediction 3");
-//   console.log(formattedPrompt);
-// };
-
-// const prediction4 = async () => {
-//   const template =
-//     "You are a helpful assistant that translates {input_language} to {output_language}.";
-//   const humanTemplate = "{text}";
-
-//   const chatPrompt = ChatPromptTemplate.fromMessages([
-//     ["system", template],
-//     ["human", humanTemplate],
-//   ]);
-
-//   const formattedChatPrompt = await chatPrompt.formatMessages({
-//     input_language: "English",
-//     output_language: "French",
-//     text: "I love programming.",
-//   });
-
-//   const llmResult = await llm.predictMessages(formattedChatPrompt);
-
-//   console.log(llmResult);
-// };
-
-// const prediction5 = async () => {
-//   /**
-//    * Parse the output of an LLM call to a comma-separated list.
-//    */
-//   class CommaSeparatedListOutputParser extends BaseOutputParser<string[]> {
-//     getFormatInstructions(
-//       options?: FormatInstructionsOptions | undefined
-//     ): string {
-//       throw new Error("Method not implemented.");
-//     }
-//     lc_namespace: string[] = [];
-//     async parse(text: string): Promise<string[]> {
-//       return text.split(",").map(item => item.trim());
-//     }
-//   }
-
-//   const template = `You are a helpful assistant who generates comma separated lists.
-// A user will pass in a category, and you should generate 5 objects in that category in a comma separated list.
-// ONLY return a comma separated list, and nothing more.`;
-
-//   const humanTemplate = "{text}";
-
-//   /**
-//    * Chat prompt for generating comma-separated lists. It combines the system
-//    * template and the human template.
-//    */
-//   const chatPrompt = ChatPromptTemplate.fromMessages([
-//     ["system", template],
-//     ["human", humanTemplate],
-//   ]);
-
-//   const model = new ChatOpenAI({});
-//   const parser = new CommaSeparatedListOutputParser();
-
-//   const chain = chatPrompt.pipe(model).pipe(parser);
-
-//   const result = await chain.invoke({
-//     text: "colors",
-//   });
-
-//   console.log(result);
-// };
-
-// const prediction6 = async () => {
-//   const template =
-//     "You are a helpful assistant that translates {input_language} to {output_language}.";
-//   const systemMessagePrompt =
-//     SystemMessagePromptTemplate.fromTemplate(template);
-//   const humanTemplate = "{text}";
-//   const humanMessagePrompt =
-//     HumanMessagePromptTemplate.fromTemplate(humanTemplate);
-
-//   const chatPrompt = ChatPromptTemplate.fromMessages<{
-//     input_language: string;
-//     output_language: string;
-//     text: string;
-//   }>([systemMessagePrompt, humanMessagePrompt]);
-
-//   console.log(chatPrompt);
-// };
-
-// const partial1 = async () => {
-//   const prompt = new PromptTemplate({
-//     template: "{foo}{bar}",
-//     inputVariables: ["foo", "bar"],
-//   });
-
-//   const partialPrompt = await prompt.partial({
-//     foo: "foo",
-//   });
-
-//   const formattedPrompt = await partialPrompt.format({
-//     bar: "baz",
-//   });
-
-//   console.log(formattedPrompt);
-// };
-
-// const partial2 = async () => {
-//   const prompt = new PromptTemplate({
-//     template: "{foo}{bar}",
-//     inputVariables: ["bar"],
-//     partialVariables: {
-//       foo: "foo",
-//     },
-//   });
-
-//   const formattedPrompt = await prompt.format({
-//     bar: "baz",
-//   });
-
-//   console.log(formattedPrompt);
-// };
-
-// const partial3 = async () => {
-//   const getCurrentDate = () => {
-//     return new Date().toISOString();
-//   };
-
-//   const prompt = new PromptTemplate({
-//     template: "Tell me a {adjective} joke about the day {date}",
-//     inputVariables: ["adjective", "date"],
-//   });
-
-//   const partialPrompt = await prompt.partial({
-//     date: getCurrentDate,
-//   });
-
-//   const formattedPrompt = await partialPrompt.format({
-//     adjective: "funny",
-//   });
-// };
-
-// const prediction7 = async () => {
-//   const model = new OpenAI({temperature: 1});
-//   const controller = new AbortController();
-
-//   // Call `controller.abort()` somewhere to cancel the request.
-
-//   const res = await model.call(
-//     "What would be a good company name a company that makes colorful socks?",
-//     {signal: controller.signal}
-//   );
-
-//   console.log(res);
-// };
-
-// const prediction8 = async () => {
-//   const model = new OpenAI({
-//     openAIApiKey: process.env.OPENAI_API_KEY,
-//     temperature: 0.9,
-//     maxRetries: 10,
-//     maxConcurrency: 5,
-//     cache: true,
-//     maxTokens: 25,
-//     streaming: true,
-//   });
-
-//   const response = await model.call("Tell me a joke.", {
-//     callbacks: [
-//       {
-//         handleLLMNewToken(token: string) {
-//           console.log({token});
-//         },
-//       },
-//     ],
-//   });
-//   console.log(response);
-// };
-
-// const prediction9 = async () => {
-//   const model = new OpenAI({
-//     callbacks: [
-//       {
-//         handleLLMStart: async (llm: Serialized, prompts: string[]) => {
-//           console.log(JSON.stringify(llm, null, 2));
-//           console.log(JSON.stringify(prompts, null, 2));
-//         },
-//         handleLLMEnd: async (output: LLMResult) => {
-//           console.log(JSON.stringify(output, null, 2));
-//         },
-//         handleLLMError: async (err: Error) => {
-//           console.error(err);
-//         },
-//       },
-//     ],
-//   });
-
-//   await model.call(
-//     "What would be a good company name a company that makes colorful socks?"
-//   );
-// };
-
-// const prediction10 = async () => {
-//   // With a `StructuredOutputParser` we can define a schema for the output.
-//   const parser = StructuredOutputParser.fromNamesAndDescriptions({
-//     answer: "answer to the user's question",
-//     source: "source used to answer the user's question, should be a website.",
-//   });
-
-//   const formatInstructions = parser.getFormatInstructions();
-
-//   const prompt = new PromptTemplate({
-//     template:
-//       "Answer the users question as best as possible.\n{format_instructions}\n{question}",
-//     inputVariables: ["question"],
-//     partialVariables: {format_instructions: formatInstructions},
-//   });
-
-//   const model = new OpenAI({temperature: 0});
-// };
-
-// // We can construct an LLMChain from a PromptTemplate and an LLM.
-// const model = new OpenAI({temperature: 0});
-// const prompt = PromptTemplate.fromTemplate(
-//   "What is a good name for a company that makes {product}?"
-// );
-
-// const cheerioTest = async () => {
-//   const loader = new CheerioWebBaseLoader("https://news.ycombinator.com/news");
-//   const docs = await loader.load();
-
-//   console.log(docs);
-// };
-
-// const puppeteer = async () => {
-//   /**
-//    * Loader uses `page.evaluate(() => document.body.innerHTML)`
-//    * as default evaluate function
-//    **/
-//   const loader = new PuppeteerWebBaseLoader(
-//     "https://news.ycombinator.com/news",
-//     {
-//       gotoOptions: {
-//         waitUntil: "domcontentloaded",
-//       },
-//       /** Pass custom evaluate, in this case you get page and browser instances */
-//       async evaluate(page: Page, browser: Browser) {
-//         await page.waitForResponse("https://news.ycombinator.com/jobs");
-
-//         const result = await page.evaluate(() => document.body.innerHTML);
-//         return result;
-//       },
-//     }
-//   );
-//   const docs = await loader.load();
-
-//   console.log(docs);
-// };
 
 const recursiveUrlLoader = async () => {
-  const urlSequence = RecursiveCharacterTextSplitter.fromLanguage("html").pipe(
-    new HtmlToTextTransformer()
-  );
+  /**
+   * The transformer responsible for transforming the html
+   */
+  const urlTransformer = RecursiveCharacterTextSplitter.fromLanguage(
+    "html"
+  ).pipe(new HtmlToTextTransformer());
 
-  // Extract information from url
+  /**
+   * The loader responsible for loading the html files recursively
+   * from the url
+   */
   const urlLoader = new RecursiveUrlLoader(
-    "https://js.l/docs/get_started/introduction",
+    "https://js.langchain.com//docs/get_started/introduction",
     {
       extractor: compile({wordwrap: 130}),
       maxDepth: 2,
@@ -353,22 +36,23 @@ const recursiveUrlLoader = async () => {
       preventOutside: true,
     }
   );
+
   try {
     // Parse html to text
     const urlDocs = R.pipe(
       async () => await urlLoader.load(),
       async _urlDocs => {
-        return await urlSequence.invoke(await _urlDocs);
+        return await urlTransformer.invoke(await _urlDocs);
       }
     );
 
     // Extract information from pdf
     const pdfDocs = R.pipe(
-      pdfPath => new PDFLoader(pdfPath),
+      (pdfPath: string) => new PDFLoader(pdfPath),
       async pdfLoader => await pdfLoader.load()
     );
 
-    const saveVector = R.pipe(
+    const saveVectorStore = R.pipe(
       async (pathToSave: string) => {
         const mergedDocs = R.concat(
           await pdfDocs("./test-pdf.pdf"),
@@ -376,8 +60,8 @@ const recursiveUrlLoader = async () => {
         );
 
         return {
-          pathToSave,
           mergedDocs,
+          pathToSave,
         };
       },
       async pipedObject => {
@@ -410,9 +94,7 @@ const recursiveUrlLoader = async () => {
       }
     );
 
-    const dir = "vector-store";
-
-    await saveVector(path.join(process.cwd(), dir));
+    await saveVectorStore(path.join(process.cwd(), "vector-store"));
   } catch (error) {
     console.error(error);
   }
@@ -420,7 +102,6 @@ const recursiveUrlLoader = async () => {
 
 recursiveUrlLoader();
 
-// change!
 const bobTest = async () => {
   const CUSTOM_QUESTION_GENERATOR_CHAIN_PROMPT = `Given the following conversation and a follow up question, return the conversation history excerpt that includes any relevant context to the question if it exists and rephrase the follow up question to be a standalone question.
   Chat History:
@@ -472,8 +153,7 @@ const bobTest = async () => {
     "I have a friend called Bob. He's 28 years old. He'd like to know who Eduardo is?";
 
   const res = await chain.call({
-    question:
-      "I have a friend called Bob. He's 28 years old. He'd like to know who Eduardo is?",
+    question,
     chat_history: "",
   });
 
