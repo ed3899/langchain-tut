07b426bdff04ee14310ac8af167ad7cb12083a4e
Eduardo Casanova <53999059+ed3899@users.noreply.github.com>
2023-10-09 16:24:32 +0000

    check

--- main.ts
+++ main.ts
@@ -373,43 +373,90 @@ const recursiveUrlLoader = async () => {
     await vectorStore.save(path.join(process.cwd(), dir));
 
     const loadedVectorStore = await HNSWLib.load(dir, embeddingsModel);
-    // TODO need filter out when there are no results regarding one query
-    // const result = await loadedVectorStore.similaritySearch("langchain");
     const retriever = loadedVectorStore.asRetriever();
 
-    const llm = new ChatOpenAI({
-      openAIApiKey: process.env.OPENAI_API_KEY,
-      temperature: 0.3,
-      maxRetries: 10,
-      maxConcurrency: 5,
-      n: 1,
-    });
-
-    const prompt = new PromptTemplate({
-      inputVariables: ["question"],
-      template:
-        "Given the question {question}, answer in a short sentence.",
-    });
-
-    const chain = ConversationalRetrievalQAChain.fromLLM(llm, retriever, {
-      memory: new BufferMemory({
-        memoryKey: "chat_history", // Must be set to "chat_history"
-        inputKey: "question",
-        outputKey: "text",
-      }),
-      questionGeneratorChainOptions: {
-        template: "Given the question {question}, answer in a short sentence. In case you don't have enough information just answer 'I don't have access to that information, sorry.'",
-      }
-    });
-
-    const res = await chain.call({
-      question: "What is langchain?",
-    });
-
-    console.log(res);
+    retriever.getRelevantDocuments("Langchain");
   } catch (error) {
     console.error(error);
   }
 };
 
-recursiveUrlLoader();
+// recursiveUrlLoader();
+
+const bobTest = async () => {
+  const CUSTOM_QUESTION_GENERATOR_CHAIN_PROMPT = `Given the following conversation and a follow up question, return the conversation history excerpt that includes any relevant context to the question if it exists and rephrase the follow up question to be a standalone question.
+  Chat History:
+  {chat_history}
+  Follow Up Input: {question}
+  Your answer should follow the following format:
+  \`\`\`
+  Use the following pieces of context to answer the users question.
+  If you don't know the answer, just say that you don't know, don't try to make up an answer.
+  ----------------
+  <Relevant chat history excerpt as context here>
+  Standalone question: <Rephrased question here>
+  \`\`\`
+  Your answer:`;
+
+  const model = new ChatOpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    modelName: "gpt-3.5-turbo",
+    temperature: 0,
+    maxRetries: 10,
+    timeout: 20000,
+  });
+
+  const embeddingsModel = new OpenAIEmbeddings({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    modelName: "text-embedding-ada-002",
+  });
+
+  const dir = "vector-store";
+  const loadedVectorStore = await HNSWLib.load(dir, embeddingsModel);
+
+  const chain = ConversationalRetrievalQAChain.fromLLM(
+    model,
+    loadedVectorStore.asRetriever(),
+    {
+      memory: new BufferMemory({
+        memoryKey: "chat_history",
+        inputKey: "question",
+        outputKey: "text",
+        returnMessages: true,
+      }),
+      questionGeneratorChainOptions: {
+        template: CUSTOM_QUESTION_GENERATOR_CHAIN_PROMPT,
+      },
+    }
+  );
+
+  const question =
+    "I have a friend called Bob. He's 28 years old. He'd like to know who Eduardo is?";
+
+  const res = await chain.call({
+    question:
+      "I have a friend called Bob. He's 28 years old. He'd like to know who Eduardo is?",
+    chat_history: "",
+  });
+
+  console.log(res);
+
+  let chatHistory = `${question}\n${res.text}`;
+
+  const res2 = await chain.call({
+    question: "How old is Bob?",
+    chat_history: chatHistory,
+  });
+
+  console.log(res2);
+  chatHistory = `${chatHistory}\n${res2.text}`;
+
+  const res3 = await chain.call({
+    question: "What is langchain?",
+    chat_history: chatHistory,
+  });
+
+  console.log(res3);
+};
+
+bobTest();
